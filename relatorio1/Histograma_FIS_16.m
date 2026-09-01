fb14467ac99afb5ea64ce9b415d1268ca473709b
caio-ishida <caioschaden@gmail.com>
2026-09-01 11:05:38 -0300

    ah

--- relatorio1/Histograma_FIS_16.m
+++ relatorio1/Histograma_FIS_16.m
@@ -2,55 +2,87 @@ clear;
 clc;
 close all;
 
-%Fotodiodo
-arquivo = 'dados.txt';
+arquivos = {'dados_cb.txt', 'dados_cm.txt'};
 
 %Limites dos gráficos
-graf_min = 1.188;
-graf_max = 1.194;
+graf_min = 11.3;
+graf_max = 12.1;
 
 %Distribuição gaussiana
 function y = norm(x, n, sigma, data, binwidth)
-y = normpdf(x, n, sigma) * length(data) * binwidth;
+    y = normpdf(x, n, sigma) * length(data) * binwidth;
 end
 
-data = readmatrix(arquivo);
+%Função nula
+function y = nulo(x)
+    y = 0 * x;
+end
 
-n_exp = mean(data);
-sigma_expec = std(data);
+%Plotando os histrogramas
+for i = 1:length(arquivos)
 
-f = figure;
-f.Color = 'w';
-f.Name = arquivo;
+    data = readmatrix(arquivos{i}, 'NumHeaderLines', 1);
+    
+    n_exp = mean(data);
+    sigma_expec = std(data);
 
-n = length(data);
-k = ceil(1 + log2(n));
+    f = figure;
+    f.Color = 'w';
+    f.Name = arquivos{i};
 
-h = histogram(data, k);
+    n = length(data);
+    k = ceil(1 + log2(n));
 
-y_hist = h.Values;
-x_hist = (h.BinEdges(1:(end - 1)) + h.BinEdges(2:end)) * 0.5;
+    h = histogram(data, k);
 
-err = - y_hist + norm(x_hist, n_exp, sigma_expec, data, h.BinWidth);
+    y_hist(i,:) = h.Values;
+    x_hist(i,:) = (h.BinEdges(1:(end - 1)) + h.BinEdges(2:end)) * 0.5;
 
-hold on;
+    err(i,:) = - y_hist(i,:) + norm(x_hist(i,:), n_exp, sigma_expec, data, h.BinWidth);
 
-x = linspace(graf_min, graf_max, 200);
-g = plot(x, norm(x, n_exp, sigma_expec, data, h.BinWidth), ...
+    hold on;
+
+    x = linspace(graf_min, graf_max, 200);
+    g = plot(x, norm(x, n_exp, sigma_expec, data, h.BinWidth), ...
         'r', 'LineWidth', 1.5);
 
-legend([h g], {'Dados experimentais', 'Distribuição Gaussiana'}, ...
+    legend([h g], {'Dados experimentais', 'Distribuição Gaussiana'}, ...
         'FontSize', 13, 'Location', 'best');
 
-axis([graf_min graf_max 0 35]);
+    axis([graf_min graf_max 0 35]);
 
-ax = gca;
-ax.XAxis.FontSize = 14;
-ax.XMinorTick = 'on';
-ax.YMinorTick = 'on';
-ax.YAxis.FontSize = 14;
+    ax = gca;
+    ax.XAxis.FontSize = 14;
+    ax.XMinorTick = 'on';
+    ax.YMinorTick = 'on';
+    ax.YAxis.FontSize = 14;
 
-tx = xlabel('\it{T} (s)');
-ty = ylabel('Frequência');
-tx.FontSize = 16;
-ty.FontSize = 16;
+    tx = xlabel('\it{10T} (s)');
+    ty = ylabel('Frequência');
+    tx.FontSize = 16;
+    ty.FontSize = 16;
+end
+
+%Calculandos os coefs de determinação
+for i = 1:2
+    r2(i) = sum(err(i,:) .^ 2) / sum((y_hist(i,:) - mean(y_hist(i,:))).^2);
+    disp(r2(i));
+end
+
+%Plotando os desvios
+ferr = figure;
+tiledlayout(1,2);
+
+for i = 1:2
+    nexttile
+    hold on
+    box on
+
+    p = plot(x_hist(i, :), err(i, :), 'b.', MarkerSize=17);
+    plot(x, nulo(x), '--');
+    legend('Desvios', 'Desvio nulo', fontsize = 13);
+    xlabel('$10T$ (s)', Interpreter='latex', FontSize=16);
+    ylabel('$f_{Gauss}-f_{hist}$', Interpreter='latex', FontSize=16);
+    ylim([-6 6]);
+end
+
